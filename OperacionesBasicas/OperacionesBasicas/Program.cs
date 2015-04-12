8933614038d269e95710ebfec6ac7507ad8f1176
Luis Alexander Aldazabal Gil <berczeck_400@hotmail.com>
2015-04-12 01:06:14 -0500

    Solucion inicial comandos LDAP DirectoryServices

---
+++ OperacionesBasicas/OperacionesBasicas/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace OperacionesBasicas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                var servicioAd = new ServicioActiveDirectory();
+
+                servicioAd.ObtenerInformacionUsuario("testig");
+                servicioAd.Autenticar("testig","testig1");
+
+                servicioAd.CambiarContrasena("testig", "tetsig1", "nuevoTest");
+
+                servicioAd.ResetearContrasena("testig", "nuevoTest");
+        }
+    }
+}
---
+++ OperacionesBasicas/OperacionesBasicas/ServicioActiveDirectory.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.DirectoryServices;
+
+namespace OperacionesBasicas
+{
+    public class ServicioActiveDirectory
+    {
+        public void Autenticar(string usuario, string contrasena)
+        {
+            var tipoAutenticacion =
+                AuthenticationTypes.None | AuthenticationTypes.FastBind;
+
+            var identificador =
+                string.Format("CN={0},OU=ADAM users,DC=prueba,DC=adam", usuario);
+
+            using (var entry =
+                new DirectoryEntry("LDAP://localhost:389/OU=ADAM users,DC=prueba,DC=adam",
+                    identificador,
+                    contrasena,
+                    tipoAutenticacion))
+            {
+                object nativeObject = entry.NativeObject;
+            }
+        }
+
+        public void CambiarContrasena(string usuario, string contrasenaAntigua, string contrasenaNueva)
+        {
+            var dn =
+                string.Format("LDAP://localhost:389/CN={0},OU=ADAM users,DC=prueba,DC=adam", usuario);
+
+            var tipoAutenticacion =
+                AuthenticationTypes.Signing | AuthenticationTypes.Sealing
+                | AuthenticationTypes.Secure;
+
+            using (var entry = new DirectoryEntry(dn, null, null, tipoAutenticacion))
+            {
+
+                entry.RefreshCache();
+
+                entry.Options.PasswordPort = 389;
+                entry.Options.PasswordEncoding = PasswordEncodingMethod.PasswordEncodingClear;
+                ;
+
+                entry.Invoke("ChangePassword", new object[] {contrasenaAntigua, contrasenaNueva});
+
+                entry.CommitChanges();
+            }
+        }
+
+        public void ResetearContrasena(string usuario, string contrasenaNueva)
+        {
+            var dn =
+                string.Format("LDAP://localhost:389/CN={0},OU=ADAM users,DC=prueba,DC=adam", usuario);
+
+            var tipoAutenticacion =
+                AuthenticationTypes.Signing | AuthenticationTypes.Sealing
+                | AuthenticationTypes.Secure;
+
+            using (var entry = new DirectoryEntry(dn, null, null, tipoAutenticacion))
+            {
+
+                entry.RefreshCache();
+
+                entry.Options.PasswordPort = 389;
+                entry.Options.PasswordEncoding = PasswordEncodingMethod.PasswordEncodingClear;
+                ;
+
+                entry.Invoke("SetPassword", new object[] { contrasenaNueva });
+
+                entry.CommitChanges();
+            }
+        }
+
+        public void ObtenerInformacionUsuario(string nombreUsuario)
+        {
+            using (var entryRoot = new DirectoryEntry("LDAP://localhost:389/OU=ADAM users,DC=prueba,DC=adam"))
+            {
+                using (var buscadorDirectorio = new DirectorySearcher(entryRoot))
+                {
+
+                    buscadorDirectorio.Filter =
+                        string.Format("(&(objectCategory=person)(objectClass=user)(cn={0}))", nombreUsuario);
+
+                    buscadorDirectorio.PropertiesToLoad.Add("businessCategory");
+                    buscadorDirectorio.PropertiesToLoad.Add("url");
+                    buscadorDirectorio.PropertiesToLoad.Add("mail");
+                    buscadorDirectorio.PropertiesToLoad.Add("company");
+                    buscadorDirectorio.PropertiesToLoad.Add("name");
+                    buscadorDirectorio.PropertiesToLoad.Add("distinguishedName");
+
+                    SearchResult resultado = buscadorDirectorio.FindOne();
+
+                    Console.WriteLine("DistinguishedName: {0}", resultado.Properties["distinguishedName"][0]);
+                    Console.WriteLine("BusinessCategory: {0}", resultado.Properties["businessCategory"][0]);
+                    Console.WriteLine("Url: {0}", resultado.Properties["url"][0]);
+                    Console.WriteLine("Mail: {0}", resultado.Properties["mail"][0]);
+                    Console.WriteLine("Company: {0}", resultado.Properties["company"][0]);
+                    Console.WriteLine("Name: {0}", resultado.Properties["name"][0]);
+                }
+            }
+        }
+    }
+}
